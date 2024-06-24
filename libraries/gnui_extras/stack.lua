79965b705ffa84a77070b56487f8f40f98b32e4b
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-06-25 00:03:37 +0800

    new stack element

---
+++ libraries/gnui_extras/stack.lua
@@ -0,0 +1,55 @@
+---@diagnostic disable: assign-type-mismatch
+local gnui = require("libraries.gnui")
+
+local container = require("libraries.gnui.primitives.container")
+local element = require("libraries.gnui.primitives.element")
+
+---@class GNUI.stack : GNUI.container
+---@field type boolean
+---@field Margin number
+local Stack = {}
+
+Stack.__index = function (t,i)
+   return rawget(t,"parent_class") and rawget(t._parent_class,i) or rawget(t,i) or Stack[i] or container[i] or element[i]
+end
+Stack.__type = "GNUI.element.container.stack"
+
+
+function Stack:updateDimensions()
+   local size = vectors.vec2()
+   local minimum_sizes = {}
+   for i, child in pairs(self.Children) do
+      if child.cache.final_minimum_size_changed or not child.cache.final_minimum_size then
+         child.cache.final_minimum_size_changed = false
+         local min = child:getMinimumSize()
+         size = size + min
+         minimum_sizes[i] = min
+      else
+         minimum_sizes[i] = child.cache.final_minimum_size
+      end
+   end
+   self:_updateDimensions()
+   if not self.cache.final_stack_size or self.cache.final_stack_size ~= size then
+      self.cache.final_stack_size = size
+      self.Dimensions = vectors.vec4(self.Dimensions.x,self.Dimensions.y,self.Dimensions.z,self.Dimensions.y + size.y)
+      local y = 0
+      for i, child in pairs(self.Children) do
+         child:setDimensions(0,y,0,y):setAnchor(0,0,1,0)
+         y = y + minimum_sizes[i].y + self.Margin
+         child:updateDimensions()
+      end
+   end
+end
+
+function Stack.new()
+   ---@type GNUI.stack
+   local new = container.new()
+   new._parent_class = Stack
+   new.type = "stack"
+   new.Margin = 1
+   return new
+end
+
+
+
+return Stack
