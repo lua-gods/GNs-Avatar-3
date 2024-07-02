24e058329002fb8d362b5d1f22a894d6a7154c6f
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-07-03 03:32:08 +0800

    new anti spam + more raw json utilities

--- libraries/rawjsonUtils.lua
+++ libraries/rawjsonUtils.lua
@@ -9,24 +9,31 @@ local function copyTable(tbl)
 end
 
 ---@param component table
----@param pattern string
+---@param pattern string?
 ---@param filter fun(component: table)
-function api.fragment(component,pattern,filter)
+function api.filterPattern(component,pattern,filter)
+   if not component then return end
    if component[1] then -- is an array
       for i = 1, #component, 1 do
-         component[i] = api.fragment(component[i],pattern,filter)
+         component[i] = api.filterPattern(component[i],pattern,filter)
       end
    else
       -- recursion
       if component.extra then
-         api.fragment(component.extra,pattern,filter)
+         api.filterPattern(component.extra,pattern,filter)
       end
       
       if type(component) == "string" then -- 1.20.3 fix
          component = {text=component}
       end
       -- component processing
-      local a, b = component.text:find(pattern)
+      local a, b
+      if pattern then
+         a, b  = component.text:find(pattern)
+      else
+         a = 1
+         b = -1
+      end
       if a and not component.antiTamper then -- split found
          local ca,cb,cc = component.text:sub(1,a-1),component.text:sub(a,b),component.text:sub(b+1,-1)
          local ta,tb,tc = copyTable(component),copyTable(component),copyTable(component)
@@ -41,10 +48,89 @@ function api.fragment(component,pattern,filter)
          end
          component.extra = {{text=""},ta,tb,tc}
          component.text = ""
-         api.fragment(tc,pattern,filter)
+         api.filterPattern(tc,pattern,filter)
       end
    end
    return component
+end
+
+---@param component table
+---@param func function
+function api.recursiveLoopComponent(component,func)
+   if not component then return end
+   if component[1] then -- is an array
+      for i = 1, #component, 1 do
+         component[i] = api.recursiveLoopComponents(component[i])
+      end
+   else
+      -- recursion
+      if component.extra then
+         api.recursiveLoopComponents(component.extra)
+      end
+      
+      if type(component) == "string" then -- 1.20.3 fix
+         component = {text=component}
+      end
+      -- component processing
+      if func then
+         func(component)
+      end
+   end
+   return component
+end
+
+---@param component table
+---@param prefix table
+function api.insertPrefix(component,prefix)
+   if not component then return end
+   local copy = copyTable(component)
+   for key, value in pairs(component) do
+      component[key] = nil
+   end
+   component.extra = {prefix,copy}
+   component.text = ""
+   return component
+end
+
+function api.insertSuffix(component,suffix)
+   if not component then return end
+   for key, value in pairs(component) do
+      component[key] = nil
+   end
+   component.extra = {copyTable(component),suffix}
+   return component
+end
+
+---Gets the component at the given index
+---@param component table
+---@param index integer
+function api.getIndexComponent(component,index)
+   if not component then return end
+   if component[1] then -- is an array
+      for i = 1, #component, 1 do
+         local returned = api.getIndexComponent(component[i],index)
+         if returned then
+            return returned
+         end
+      end
+   else
+      -- recursion
+      if component.extra then
+         local returned = api.getIndexComponent(component.extra,index)
+         if returned then
+            return returned
+         end
+      end
+      
+      if type(component) == "string" then -- 1.20.3 fix
+         component = {text=component}
+      end
+      -- component processing
+      index = index - 1
+      if index == 0 then
+         return component
+      end
+   end
 end
 
 return api
--- scriptHost/chat.lua
+++ scriptHost/chat.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: assign-type-mismatch, missing-fields
 
 ---@type chatscript.post_data[]
-local msg_data = {}
+local history = {}
 
 ---@class chatscript.post_data
 ---@field pre_json table
@@ -18,10 +18,20 @@ local msg_data = {}
 ---@field time number
 ---@field with table
 
+local function realizeChangesOnIndex(index)
+   if history[index] then
+      ---@diagnostic disable-next-line: param-type-mismatch
+         host:setChatMessage(index,toJson(history[index].post_json))
+   end
+end
+
 local utils = require("libraries.rawjsonUtils")
 local env = {math=math}
 
----@type table<integer,{post: fun(message: chatscript.post_data), pre: fun(message: chatscript.pre_data)}>
+-- Anti spam
+local last_message = ""
+local spam_combo = 1
+
 local message_filters = {
    -- Theme
    {
@@ -35,20 +45,66 @@ local message_filters = {
          end
       end,
    },
+   -- Anti Spam
+   {
+      ---@param message chatscript.post_data
+      post = function (message)
+         if last_message == message.plain_text then
+            spam_combo = spam_combo + 1
+            if spam_combo == 2 then
+               utils.insertPrefix(utils.getIndexComponent(history[2].post_json,1),{text = "(x"..spam_combo..") ",color="red"})
+            else
+               if history[2] then
+                  local component = utils.getIndexComponent(history[2].post_json,1)
+                  if component then
+                     component.text = "(x"..spam_combo..") "
+                  end
+               end
+            end
+            realizeChangesOnIndex(2)
+            return true
+         else
+            last_message = message.plain_text
+            spam_combo = 1
+         end
+      end,
+   },
+   -- Copy Coordinates
+   {
+      ---@param message chatscript.post_data
+      post = function (message)
+         if message.translate == "chat.type.text" then
+            ---@param component table
+            utils.filterPattern(message.post_json,"[%d-.]+%s+[%d-.]+%s+[%d-.]+",function (component)
+               local axis = 0
+               local colors = {"red","green","blue"}
+               utils.filterPattern(component,"[%d-.]+",function (sub_component)
+                  axis = axis + 1
+                  sub_component.color = colors[axis]
+                  sub_component.clickEvent = {action = "copy_to_clipboard", value = component.text}
+                  sub_component.hoverEvent = {action = "show_text", contents = {text="Copy Coordinates to Clipboard"}}
+                  sub_component.antiTamper = true
+               end)
+            end)
+         end
+      end,
+   },
    --Calculator
    {
       ---@param message chatscript.post_data
       post = function (message)
          if message.translate == "chat.type.text" then
             ---@param component table
-            utils.fragment(message.post_json,"%[[^]]+%]",function (component)
-               local ok,result = pcall(load("return "..component.text:sub(2,-2),"meth","t",env))
-               if ok and (type(result) == "number" or not result) then
-                  if result then
-                     component.text = component.text .. " = " .. result
-                     component.clickEvent = {action = "copy_to_clipboard", value = tostring(result)}
-                     component.hoverEvent = {action = "show_text", contents = {text=tostring(result)}}
+            utils.filterPattern(message.post_json,"[%d+%-*/ ()]+",function (component)
+               if #component.text > 1 then
+                  local ok,result = pcall(load("return "..component.text,"meth","t",env))
+                  if ok and (type(result) == "number" or not result) then
+                     if result then
+                        component.clickEvent = {action = "copy_to_clipboard", value = tostring(result)}
+                        component.hoverEvent = {action = "show_text", contents = {text="it's "..result.." btw"}}
+                     end
                   end
+                  component.antiTamper = true
                   component.color = "gray"
                end
             end)
@@ -61,31 +117,11 @@ local message_filters = {
       post = function (message)
          if message.translate == "chat.type.text" then
             ---@param component table
-            utils.fragment(message.post_json,"https?://[%a%d;,/?:@&=+%$-_.!~*'()#]+",function (component)
+            utils.filterPattern(message.post_json,"https?://[%a%d;,/?:@&=+%$-_.!~*'()#]+",function (component)
                component.color = "yellow"
                component.underlined = "true"
                component.clickEvent = {action = "open_url", value = component.text}
                component.hoverEvent = {action = "show_text", contents = {text="Open URL"}}
-            end)
-         end
-      end,
-   },
-   -- Copy Coordinates
-   {
-      ---@param message chatscript.post_data
-      post = function (message)
-         if message.translate == "chat.type.text" then
-            ---@param component table
-            utils.fragment(message.post_json,"[%d-.]+%s+[%d-.]+%s+[%d-.]+",function (component)
-               local axis = 0
-               local colors = {"red","green","blue"}
-               utils.fragment(component,"[%d-.]+",function (sub_component)
-                  axis = axis + 1
-                  sub_component.color = colors[axis]
-                  sub_component.clickEvent = {action = "copy_to_clipboard", value = component.text}
-                  sub_component.hoverEvent = {action = "show_text", contents = {text="Copy Coordinates to Clipboard"}}
-                  sub_component.antiTamper = true
-               end)
             end)
          end
       end,
@@ -96,13 +132,13 @@ local message_filters = {
       post = function (message)
          if message.translate == "chat.type.text" then
             ---@param component table
-            utils.fragment(message.post_json,"[%d.-]+",function (component)
+            utils.filterPattern(message.post_json,"[%d.-]+",function (component)
                component.clickEvent = {action = "copy_to_clipboard", value = component.text}
                component.hoverEvent = {action = "show_text", contents = {text="Copy Numbers to Clipboard"}}
             end)
          end
       end,
-   }
+   },
 }
 
 local recived_messages = 0
@@ -120,27 +156,42 @@ events.CHAT_RECEIVE_MESSAGE:register(function (message, json)
          message_filters[i].pre(predata)
       end
    end
-   msg_data[recived_messages] = predata
+   
+   -- history
+   table.insert(history,recived_messages,predata)
+   if #history > 30 then
+      for i = 30, #history, 1 do
+         history[i] = nil
+      end
+   end
    return toJson(predata.pre_json)
 end)
 
 
 events.WORLD_RENDER:register(function ()
-   for i = 1, recived_messages, 1 do
+   local i = 1
+   while recived_messages >= i do
       local post_msg_data = host:getChatMessage(i)
-      
+      if not post_msg_data then return end
       local json = parseJson(post_msg_data.json)
-      local data = msg_data[i]
+      local data = history[i]
       data.time = post_msg_data.addedTime
       data.post_json = json
+      local delete = false
       for j = 1, #message_filters, 1 do
          if message_filters[j].post then
-            message_filters[j].post(data)
+            delete = delete or message_filters[j].post(data)
          end
       end
-      host:setChatMessage(i,toJson(data.post_json))
+      if delete then
+         host:setChatMessage(i)
+         table.remove(history,i)
+         recived_messages = recived_messages - 1
+      else
+         host:setChatMessage(i,toJson(data.post_json))
+         i = i + 1
+      end
    end
    recived_messages = 0
-   msg_data = {}
 end)
 
