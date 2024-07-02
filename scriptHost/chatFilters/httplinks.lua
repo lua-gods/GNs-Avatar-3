7b0cd3356baf792b622ddf66cd2506352ee36232
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-07-02 19:58:09 +0800

    more 1.20.4 fixes

--- scriptHost/chatFilters/httplinks.lua
+++ scriptHost/chatFilters/httplinks.lua
@@ -6,7 +6,7 @@ return {
          ---@param component table
          utils.fragment(message.post_json,"https?://[%a%d;,/?:@&=+%$-_.!~*'()#]+",function (component)
             component.color = "yellow"
-            component.underlined = true
+            component.underlined = "true"
             component.clickEvent = {action = "open_url", value = component.text}
             component.hoverEvent = {action = "show_text", contents = {text="Open URL"}}
          end)
