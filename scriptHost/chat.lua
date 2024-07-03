e73ff1b44f4f1a10a471355248b2d58a106aa8c4
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2024-07-03 11:04:46 +0200

    chat script 1.20.4 fix

--- scriptHost/chat.lua
+++ scriptHost/chat.lua
@@ -42,10 +42,9 @@ local message_filters = {
    {
       ---@param message chatscript.post_data
       post = function (message)
-         
          if message.translate == "chat.type.text" then
             local json = message.post_json
-            json.extra[1].text = ""
+            json.extra[1] = ""
             json.extra[3] = {text = " : ",color="gray"}
          end
       end,
