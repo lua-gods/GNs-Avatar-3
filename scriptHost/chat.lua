f3486514ac87abae1d316b26b9854cf342f86b4d
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-07-03 17:15:48 +0800

    brought back hex codes

--- scriptHost/chat.lua
+++ scriptHost/chat.lua
@@ -94,7 +94,33 @@ local message_filters = {
          end
       end,
    },
-   
+   {
+      ---@param message chatscript.post_data
+      post = function (message)
+         if message.translate == "chat.type.text" then
+            local matched = false
+            ---@param component table
+            utils.filterPattern(message.post_json,"#%x%x%x%x%x%x",function (component)
+               component.color = component.text
+               component.clickEvent = {action = "copy_to_clipboard", value = component.text}
+               component.hoverEvent = {action = "show_text", contents = {text="Copy to Clipboard"}}
+               matched = true
+               component.antiTamper = true
+            end)
+            if not matched then
+               ---@param component table
+               utils.filterPattern(message.post_json,"#%x%x%x",function (component)
+                  local r,g,b = component.text:sub(2,2),component.text:sub(3,3),component.text:sub(4,4)
+                  component.color = "#"..r..r..g..g..b..b
+                  component.clickEvent = {action = "copy_to_clipboard", value = component.text}
+                  component.hoverEvent = {action = "show_text", contents = {text="Copy to Clipboard"}}
+                  component.antiTamper = true
+               end)
+            end
+         end
+         --if message.translate == ""
+      end,
+   },
    --HTTP Links
    {
       ---@param message chatscript.post_data
