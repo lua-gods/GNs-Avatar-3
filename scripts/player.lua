e417c39d8ba839de70a0ebde96b8962b575da5ad
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-07-09 19:59:52 +0800

    icons

--- scripts/player.lua
+++ scripts/player.lua
@@ -1,5 +1,4 @@
 vanilla_model.PLAYER:setVisible(false)
-models.player.Head.Eyes:setPrimaryRenderType("EMISSIVE_SOLID")
 models.player:setPrimaryRenderType("CUTOUT")
-
 avatar:store("hair_color","5ac54f")
+renderer:setShadowRadius(0.4)
