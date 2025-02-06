4cbb417d065efa42bd70baa1cd190915576bbd87
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-02-07 01:38:05 +0800

    ladders as launchers

--- macros/Goofy Plugin/Ladders are Launchers.lua
+++ macros/Goofy Plugin/Ladders are Launchers.lua
@@ -2,15 +2,12 @@ local Macros = require("lib.macros")
 
 local jump = keybinds:fromVanilla("key.jump")
 
-return Macros.new("Bouncy World",function (events)
+return Macros.new("LaddersAreLaunchers",function (events)
 	local lvel = vec(0,0,0)
    function events.TICK()
 		local vel = vec(table.unpack(player:getNbt().Motion))
-		local accel = (vel - lvel)
-		local fvel = lvel + accel * 2
-		if accel:length() > 0.1 then
-			goofy:setVelocity(fvel)
+		if world.getBlockState(player:getPos()).id == "minecraft:ladder" and jump:isPressed() then
+			goofy:setVelocity(vel + vec(0,1,0))
 		end
-		lvel = fvel
 	end
 end),"Makes everything bouncy"
