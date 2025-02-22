5aaf3004204f3d798a9564a0bb00039a42942823
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-02-23 01:16:26 +0800

    shadow mayhem and solid entities

---
+++ macros/Useless/Shadow Mayhem.lua
@@ -0,0 +1,31 @@
+
+
+local Macros = require("lib.macros")
+
+
+return Macros.new("Shadow Mayhem",function (macro)
+	local shadow = models.player:copy("Shadowerer")
+	:setParentType("WORLD")
+	:setColor(0,0,0)
+	:scale(1,0,1)
+	models:addChild(shadow)
+	renderer:setShadowRadius(0)
+	
+	function macro.WORLD_RENDER(deltaFrame,deltaTick)
+		if not player:isLoaded() then return end
+		local dayTime = (((world.getTimeOfDay()) % 24000) / 24000 - 0.25) % 1
+    	local sunAngle = (dayTime * 2 + (0.5 - math.cos(dayTime * math.pi) / 2)) / 3 * 360
+		 local shift = math.tan(math.rad((sunAngle%180)))
+		local ppos = player:getPos(deltaTick)
+		local _,floor = raycast:block(ppos,ppos-vec(-shift*10,10,0),"COLLIDER","NONE")
+		local mat = matrices.mat4()
+		mat:scale(1,0,1):rotateY(180-player:getBodyYaw(deltaTick)):translate((floor*16):add(0,0.3,0))
+		mat.c2 = mat.c2._yzw + vec(shift,0,0,0)
+		shadow:setMatrix(mat)
+	end
+	
+	function macro.EXIT()
+		renderer:setShadowRadius()
+		shadow:remove()
+	end
+end,true),"Duplicates the player modelPart and squishes it onto the ground"
---
+++ macros/Useless/Solid Entities.lua
@@ -0,0 +1,45 @@
+
+local side2dir = {
+	north = vec(0,0,-1),
+	east = vec(1,0,0),
+	south = vec(0,0,1),
+	west = vec(-1,0,0),
+	up = vec(0,1,0),
+	down = vec(0,-1,0)
+}
+
+local Macros = require("lib.macros")
+
+return Macros.new("Solid Entities",function (macro)
+	local lvel = vec(0,0,0)
+	function macro.TICK(df,dt)
+		if player:isLoaded() then
+			local psize = player:getBoundingBox()
+			local checkSize = psize*vec(3,3,3)
+			local ppos = player:getPos(dt)
+			local pvel = player:getVelocity()
+			local entities = world.getEntities(-checkSize+ppos,checkSize+ppos) ---@type Entity[]
+			local aabb = {}
+			for _, entity in ipairs(entities) do
+				if entity:getUUID() ~= player:getUUID() then 
+					local offset = entity:getBoundingBox()
+					local pos = entity:getPos()
+					local min = pos - (offset) * vec(0.5,0,0.5) - psize.xyz
+					local max = pos + offset * vec(0.5,1,0.5) + psize.x_z
+					aabb[#aabb+1] = {min,max}
+				end
+			end
+			local _,hitpos,side,id = raycast:aabb(ppos,ppos+pvel,aabb)
+			local normal = side2dir[side]
+			if side then
+				goofy:setPos(hitpos+normal*0.1)
+				local evel = entities[id]:getVelocity()
+				goofy:setVelocity(vec(table.unpack(player:getNbt().Motion)) * (vec(1,1,1)-normal) + (evel - pvel.x_z))
+				lvel = evel
+				if host:isJumping() then
+					goofy:setVelocity(vec(table.unpack(player:getNbt().Motion)) + vec(0,0.45,0))
+				end
+			end
+		end
+	end
+end,true),"Duplicates the player modelPart and squishes it onto the ground"
